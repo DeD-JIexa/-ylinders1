a3e888312d433f7565abe88701a4db7b46a1d91e
Alex <alex3kot@outlook.com>
2020-04-13 00:37:04 +0400

    Добавьте файлы проекта.

---
+++ Laba1_vano/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Сylinders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество цилиндров:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите высоту цилиндров:");
+            double h = Convert.ToDouble(Console.ReadLine());
+            double s = Math.PI * h * n * (n + 1) * (2 * n + 1) * 3 / 2;
+            Console.WriteLine("Сумма объёмов: " + s);
+        }
+    }
+}
